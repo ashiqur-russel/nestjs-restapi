883af4a55875af482786f75d5d396afbc5c5c8b0
ashiqurras <ashiqurrussel2@gmail.com>
2022-12-12 23:58:45 +0100

    import and export prisma

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,10 @@
+import { PrismaService } from './../prisma/prisma.service';
 import { Injectable, Post } from '@nestjs/common';
+import { User, Bookmark } from '@prisma/client';
 
 @Injectable({})
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
   signup() {
     return { status: true, user: 'Ashiqur' };
   }
